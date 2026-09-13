ada7de57bc76d9d6eab7e9acdc8f64c887e9adb2
Fares Hassan <fareshassan437@gmail.com>
2026-09-13 04:26:08 +0300

    final commit

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+const mainRouter = require('./routes/main');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+// routes
+app.use('/api/v1', mainRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -18,6 +25,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
